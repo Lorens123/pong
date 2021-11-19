d456a5edec83749983037a67863d3443a1a16ca2
Lorens123 <lorens.vinge@gmail.com>
2021-11-19 20:08:31 +0100

    Första koden

---
+++ pong/Ball.cs
@@ -0,0 +1,19 @@
+using System;
+using Raylib_cs;
+public class Ball
+{
+    // Bollen till pong ska vara fyrkantig i kvadrat form, 
+    // Den ska kunna stunda på paddlarna och väggarna.
+    // När bollen nuddar väggen på sidorna ger det 1 poäng. Max 5 för att vinna med.
+    // I början av varje spel ska bollen åka till "random" håll antingen höger eller vänster. 
+    private Rectangle rect;
+    private Random generator = new Random();
+    private int size;
+
+
+
+
+
+
+
+}
--- pong/Program.cs
+++ pong/Program.cs
@@ -1,8 +1,27 @@
 ﻿using Raylib_cs;
 
 
+Raylib.InitWindow(800, 600, "Ultimate Pong Game");
 
+// Player p1 = new LeftPlayer();
+// Player p2 = new RightPlayer();
+Ball ball = new Ball();
 
+while (!Raylib.WindowShouldClose())
+{
+    // p1.Update();
+    // p2.Update();
+    ball.Update();
 
+    Raylib.BeginDrawing();
 
+    Raylib.ClearBackground(Color.BLACK);
 
+    Raylib.DrawCircle(100, 100, 100, Color.WHITE);
+
+    // p1.Draw();
+    // p2.Draw();
+    ball.Draw();
+
+    Raylib.EndDrawing();
+}
