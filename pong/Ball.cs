4492a219d5618d21f564f6a34830cbe5ef5100b7
Lorens123 <lorens.vinge@gmail.com>
2021-11-22 13:12:18 +0100

    rektangle skiss från ball klassen

--- pong/Ball.cs
+++ pong/Ball.cs
@@ -3,17 +3,21 @@ using Raylib_cs;
 public class Ball
 {
     // Bollen till pong ska vara fyrkantig i kvadrat form, 
-    // Den ska kunna stunda på paddlarna och väggarna.
+    // Den ska kunna stunda på paddlarna och på vågrätta väggarna.
     // När bollen nuddar väggen på sidorna ger det 1 poäng. Max 5 för att vinna med.
     // I början av varje spel ska bollen åka till "random" håll antingen höger eller vänster. 
     private Rectangle rect;
     private Random generator = new Random();
-    private int size;
 
 
+    private Ball()
+    {
+        rect = new Rectangle(10, 10, 10, 10);
+        Raylib.DrawRectangleRec(rect, Color.WHITE);
 
+    }
+    private void BeginningOfTheGame()
+    {
 
-
-
-
+    }
 }
